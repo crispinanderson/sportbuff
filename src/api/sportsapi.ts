9c09474a7b7d12da5b394180a59cb7baf9468dfa
Crispin Anderson <crispin.anderson@gmail.com>
2020-10-02 15:59:58 +0100

    axios call to api

--- src/api/sportsapi.ts
+++ src/api/sportsapi.ts
@@ -1,7 +1,12 @@
 
-
-// TODO implement: https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple
-
-export const getQuestions = () => { 
-    return JSON.parse(`{"response_code":0,"results":[{"category":"Sports","type":"multiple","difficulty":"medium","question":"Which of these teams is a member of the NHL era?","correct_answer":"Philadelphia Flyers","incorrect_answers":["New York Rangers","Toronto Maple Leafs","Boston Bruins"]}]}`);
- }
+import Axios from 'axios';
+// const mockdata = JSON.parse(`{"response_code":0,"results":[{"category":"Sports","type":"multiple","difficulty":"medium","question":"Which of these teams is a member of the NHL era?","correct_answer":"Philadelphia Flyers","incorrect_answers":["New York Rangers","Toronto Maple Leafs","Boston Bruins"]}]}`);
+export const getQuestions = async () => {
+    return Axios.get('https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple')
+        .then((res) => {
+            return res.data
+        })
+        .catch((err) => {
+            return err
+        })
+}
