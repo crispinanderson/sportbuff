0175c61adfc126d23a652156dae16c94620ea1bb
Crispin Anderson <crispin.anderson@gmail.com>
2020-10-03 08:06:53 +0100

    EditQuestion Type

--- src/types/Question.ts
+++ src/types/Question.ts
@@ -1,15 +1,24 @@
+import { nullLiteral } from "@babel/types";
+
 export interface QuestionResponse {
-    response_code: number;
-    results?: (Question)[] | null;
-  }
+  response_code: number;
+  results?: (Question)[] | null;
+}
 
 export interface Question {
-    category: string;
-    type: string;
-    difficulty: string;
-    question: string;
-    correct_answer: string;
-    incorrect_answers?: (string)[] | null;
-  }
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers?: (string)[] | null;
+}
+
+interface EditQuestionData extends Question {
+  index: number,
+  edited: boolean
+}
+
+export type EditQuestion = EditQuestionData | null
 
 
