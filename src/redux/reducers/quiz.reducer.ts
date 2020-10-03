15aaa26c4cd1f25f2ba60aa00739b290d1645f2a
Crispin Anderson <crispin.anderson@gmail.com>
2020-10-03 15:36:50 +0100

    split reducer to seperate file

---
+++ src/redux/reducers/quiz.reducer.ts
@@ -0,0 +1,52 @@
+
+import * as sportwebservice from '../actions/questionwebservice';
+
+import { Question, EditQuestion } from '../../types/Question';
+
+export interface QuizState {
+    questions: Question[]
+    edit: EditQuestion
+}
+const initialQuizState: QuizState = {
+    questions: [],
+    edit: null
+};
+
+export function questionReduxReducer(state = initialQuizState, action) {
+
+    switch (action.type) {
+        case sportwebservice.Types.GET_QUESTIONS_SUCCESS:
+            return {
+                questions: [
+                    ...action.payload.results,
+                    ...state.questions
+                ]
+            };
+        case sportwebservice.Types.DELETE_QUESTION_REQUEST:
+            return {
+                questions: [
+                    ...state.questions.slice(0, action.payload.index),
+                    ...state.questions.slice(action.payload.index + 1)
+                ],
+            };
+        case sportwebservice.Types.EDIT_QUESTION_REQUEST:
+            return {
+                ...state,
+                edit: {
+                    ...state.questions[action.payload.index],
+                    edited: false,
+                    index: action.payload.index
+                }
+            };
+        case sportwebservice.Types.SAVE_QUESTION_REQUEST:
+            return {
+                questions: [
+                    ...state.questions.slice(0, action.payload.index),
+                    action.payload.data,
+                    ...state.questions.slice(action.payload.index + 1)
+                ],
+            };
+        default:
+            return state;
+    }
+}
