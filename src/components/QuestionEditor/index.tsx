7dbfa57d7686eadc6fc3fc18726321578c8cb4b5
Crispin Anderson <crispin.anderson@gmail.com>
2020-10-03 08:06:07 +0100

    basic connected editor

--- src/components/QuestionEditor/index.tsx
+++ src/components/QuestionEditor/index.tsx
@@ -1,14 +1,32 @@
-import {  Grid } from "@material-ui/core";
+import { Grid, Paper } from "@material-ui/core";
 import React from "react";
+import { connect } from 'react-redux';
 
+const styles = {
+  wrapper: {
 
+  }
+}
 
-export default function QuestionEditor ()  {
+export function QuestionEditor({ dispatch, edit }) {
 
-    return(
-        <Grid xs={7}>
-          <h1>Editor (TODO)</h1>
-        </Grid>
-      );
+  return (
+    <Grid item xs={7}>
+      <h1>Editor</h1>
+      {edit &&
+        <Paper>
+
+        </Paper>}
+
+    </Grid>
+
+  );
 
 }
+
+function mapStateToProps(state: any) {
+  return { edit: state.quiz.edit };
+}
+
+
+export default connect(mapStateToProps)(QuestionEditor)
